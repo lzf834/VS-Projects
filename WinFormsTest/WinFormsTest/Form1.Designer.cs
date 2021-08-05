bc0d0bb935e516a287fc1aefdeb41efc10ca42ee
Zi Foong Lye <e0544161@u.nus.edu>
2021-08-05 12:55:28 +0800

    Changed To datagridview to display data

--- WinFormsTest/WinFormsTest/Form1.Designer.cs
+++ WinFormsTest/WinFormsTest/Form1.Designer.cs
@@ -29,50 +29,40 @@ namespace WinFormsTest
         /// </summary>
         private void InitializeComponent()
         {
-            this.tb_logname = new System.Windows.Forms.TextBox();
-            this.tb_logdetails = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tb_logname
+            // dataGridView1
             // 
-            this.tb_logname.Location = new System.Drawing.Point(12, 12);
-            this.tb_logname.Multiline = true;
-            this.tb_logname.Name = "tb_logname";
-            this.tb_logname.ReadOnly = true;
-            this.tb_logname.Size = new System.Drawing.Size(100, 49);
-            this.tb_logname.TabIndex = 0;
-            this.tb_logname.TabStop = false;
-            this.tb_logname.Text = "Machine Name:\r\nUser:\r\nDate:";
-            // 
-            // tb_logdetails
-            // 
-            this.tb_logdetails.AcceptsReturn = true;
-            this.tb_logdetails.Location = new System.Drawing.Point(118, 12);
-            this.tb_logdetails.Multiline = true;
-            this.tb_logdetails.Name = "tb_logdetails";
-            this.tb_logdetails.ReadOnly = true;
-            this.tb_logdetails.Size = new System.Drawing.Size(183, 49);
-            this.tb_logdetails.TabIndex = 1;
-            this.tb_logdetails.TabStop = false;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(303, 134);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 78);
-            this.Controls.Add(this.tb_logdetails);
-            this.Controls.Add(this.tb_logname);
+            this.ClientSize = new System.Drawing.Size(327, 158);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tb_logname;
-        private System.Windows.Forms.TextBox tb_logdetails;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- WinFormsTest/WinFormsTest/Form1.cs
+++ WinFormsTest/WinFormsTest/Form1.cs
@@ -15,9 +15,31 @@ namespace WinFormsTest
         public Form1()
         {
             InitializeComponent();
-            tb_logdetails.Text += Environment.MachineName + "\r\n";
-            tb_logdetails.Text += Environment.UserName + "\r\n";
-            tb_logdetails.Text += DateTime.Now.ToLongDateString() + "\r\n";
+            //Using textbox
+            //tb_logdetails.Text += Environment.MachineName + "\r\n";
+            //tb_logdetails.Text += Environment.UserName + "\r\n";
+            //tb_logdetails.Text += DateTime.Now.ToLongDateString() + "\r\n";
+
+            //Setting base table properties
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.RowCount = 4;
+            dataGridView1.RowHeadersWidth = 130;
+            dataGridView1.AutoSize = true;
+            dataGridView1.BackgroundColor = SystemColors.ControlLightLight;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.Columns[0].Name = "Properties";
+
+            dataGridView1.Rows[0].HeaderCell.Value = "Machine Name";
+            dataGridView1.Rows[1].HeaderCell.Value = "User Name";
+            dataGridView1.Rows[2].HeaderCell.Value = "Date";
+            dataGridView1.Rows[3].HeaderCell.Value = "Time";
+
+            dataGridView1.Rows[0].Cells[0].Value = Environment.MachineName;
+            dataGridView1.Rows[1].Cells[0].Value = Environment.UserName;
+            dataGridView1.Rows[2].Cells[0].Value = DateTime.Now.ToShortDateString();
+            dataGridView1.Rows[3].Cells[0].Value = DateTime.Now.ToShortTimeString();
         }
     }
 }
